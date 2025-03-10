3dfff8540aafb7d5a588cb45323651d7f808cebb
BLACKDEMO0 <blackdemo190@gmail.com>
2025-03-10 15:28:38 +0545

    here

--- src/app/Servicepage/page.jsx
+++ src/app/Servicepage/page.jsx
@@ -21,7 +21,6 @@ const Page = () => {
           <div className="text-5xl font-extralight mt-2">Our Services</div>
         </div>
       </div>
-<div></div>
      
       <div className="h-screen w-full">
         <Amazing />
